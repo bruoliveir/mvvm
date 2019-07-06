11d4a63f2019ebb87f4161284b399b65cbede368
Bruno Oliveira <bru.oliveir@gmail.com>
2019-07-06 18:21:22 -0300

    Creates view package

--- app/src/main/java/com/example/mvvm/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mvvm
+package com.example.mvvm.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.mvvm.R
 import com.example.mvvm.databinding.ActivityMainBinding
 import com.example.mvvm.model.Player
 import com.example.mvvm.viewmodel.GameViewModel
